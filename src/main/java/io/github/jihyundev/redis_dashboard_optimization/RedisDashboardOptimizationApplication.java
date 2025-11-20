8ffee417b08526ef1af13fe14ec7c84c35dc9830
JiHyun Jang <gihyun0818@gmail.com>
2025-11-20 19:52:02 +0900

    Initial commit

---
+++ src/main/java/io/github/jihyundev/redis_dashboard_optimization/RedisDashboardOptimizationApplication.java
@@ -0,0 +1,13 @@
+package io.github.jihyundev.redis_dashboard_optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDashboardOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDashboardOptimizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jihyundev/redis_dashboard_optimization/RedisDashboardOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jihyundev.redis_dashboard_optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDashboardOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
